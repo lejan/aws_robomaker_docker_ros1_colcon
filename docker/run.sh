aff934666f4d10cb6fa0b09f5637a99f32cb7227
lejan <alem.leandro@gmail.com>
2021-10-20 14:02:37 -0300

    Add docker files: - Dockerfile - Dockerfile with nvidia support - makefile to use with make build and make run

--- docker/run.sh
+++ docker/run.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
-docker="bionic_melodic_gaz9_chapa_robomaker_starting_nvidia"
+# Absolute path to this script, e.g. /home/user/bin/run_script.sh
+RUN_SCRIPT=$(readlink -f "$0")
+# Absolute path this script is in, thus /home/user/bin
+RUN_SCRIPT_PATH=$(dirname "$RUN_SCRIPT")
+
+docker="bionic_melodic_gaz9_robomaker_starting_nvidia"
 uid=$(id -u $USER)
-repository="/home/lejan/Documents/21-10-06_MISO/21-10-20_ChapaDocker/chapa_colcon_ws"
-workspace="/home/lejan/project_ws/"
+repository="$RUN_SCRIPT_PATH/../src"
+workspace="/home/$USER/project_ws"
+
 
 xhost +local:root
 
 docker run -it --rm --env="DISPLAY" --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
            --volume="$HOME/.Xauthority:/root/.Xauthority:rw" --name=$docker \
            --privileged --ipc="host" --cap-add=IPC_LOCK --cap-add=sys_nice --user $uid:$uid \
-           -e HOME=/home/lejan -e ROS_HOSTNAME=localhost -e ROS_MASTER_URI=http://localhost:11311 \
-           --workdir $workspace --volume $repository:$workspace \
+           -e ROS_HOSTNAME=localhost -e ROS_MASTER_URI=http://localhost:11311 \
+           --workdir $workspace --volume $repository:$workspace/src \
            --network="host" --gpus all $docker
            
 xhost -local:root
