c6b27702450d90c795624032805c545895a75ce1
lejan <alem.leandro@gmail.com>
2021-10-20 14:50:32 -0300

    Add post_install script to run in first place

---
+++ script/post_install.sh
@@ -0,0 +1,16 @@
+sudo apt-get update
+sudo apt-get upgrade
+rosdep update
+rosdep install --from-paths src --ignore-src -r -y
+sudo apt install python3-pip -y
+sudo pip3 install colcon-ros-bundle
+sudo apt-get install python3-apt -y
+sudo pip3 install -U setuptools pip
+sudo apt-get install wget
+wget http://packages.osrfoundation.org/gazebo.key -O - | sudo apt-key add -
+colcon build
+cd /
+./ros_entrypoint.sh
+cd ~/project_ws
+rosdep update
+colcon bundle
